5f0a103dc5b1cc3817db13d532ab68e231bc189d
anshsangal12 <sangalansh4561@gmail.com>
2025-11-18 12:18:30 +0530

    Create Experiment2.java

---
+++ Experiment2.java
@@ -0,0 +1,21 @@
+abstract class Animal {
+    abstract void sound();
+
+    void eat() {
+        System.out.println("Animal eats food");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Experiment2 {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.sound();
+        a.eat();
+    }
+}
